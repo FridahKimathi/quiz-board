e6fe0570abcc6fc314963c40e5d14f87498c2059
fridah kim <fredakim18@gmail.com>
2018-12-09 20:54:46 +0300

    Added js and linked jquery to the project

---
+++ js/scripts.js
@@ -0,0 +1,55 @@
+// BUSINESS LOGIC
+var sum = 0;
+function scoreCalculator(userValue) {
+  var names = ["js", "year", "framework","function","dom","link"];
+  names.forEach(function(name) {
+    var userValue = $("input:radio[name=" + name + "]:checked").val();
+    if(userValue === "10") {
+      sum+=10;
+    }
+  });
+}
+
+
+// USER INTERFACE LOGIC
+$(function() {
+  $("#start-btn").click(function(event) {
+    event.preventDefault();
+    var userFirstName = $("#firstName").val();
+    var userSecondName = $("#secondName").val();
+    if(userFirstName.length === 0 || userSecondName.length === 0) {
+      $("#no-name").show();
+      $("#firstName").addClass("enter-name");
+      $("#secondName").addClass("enter-name");
+    } else {
+      $("#intro").hide();
+      $("#form1").show();
+    }
+  })
+  $("#arrow-right").click(function() {
+    $("#form1").hide();
+    $("#form2").show();
+  })
+  $("#arrow-left").click(function() {
+    $("#form1").show();
+    $("#form2").hide();
+  })
+  $("#submit").click(function(event) {
+    event.preventDefault();
+    $("#score").show();
+    $("#form2").hide();
+    scoreCalculator();
+    var userFirstName = $("#firstName").val();
+    var userSecondName = $("#secondName").val();
+    if(sum <= 30) {
+      $(".name-display").text(`Nice try!`);
+    } else {
+      $(".name-display").text(`Congratulations!`);
+    }
+    $(".scoreDisplay").text(sum + "/20");
+  })
+  $("#retake").click(function(event) {
+    event.preventDefault();
+    location.reload();
+  })
+})
